6a287e7a47b4974595962dd435b84a1e84c1bd25
J.yeu <chen@jyeu.xyz>
2024-12-28 12:20:36 +0800

    feat(ui): Add header component to brew service view

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import "./App.css";
 import { Background } from "./views/background.tsx";
+import { BrewServiceHeader } from "./views/brew-service-header.tsx";
 import { BrewServiceListView } from "./views/brew-service-list.tsx";
 
 function App() {
 	return (
 		<Background>
 			<>
-				<div className="mb-4 flex flex-nowrap items-center justify-between">
-					<p className="relative bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text font-bold text-transparent">
-						Homebrew Services
-					</p>
-					<div>button</div>
-				</div>
+				<BrewServiceHeader />
 				<BrewServiceListView />
 			</>
 		</Background>
---
+++ src/views/brew-service-header.tsx
@@ -0,0 +1,10 @@
+export function BrewServiceHeader() {
+	return (
+		<div className="mb-4 flex flex-nowrap items-center justify-between">
+			<p className="relative bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text font-bold text-transparent">
+				Homebrew Services
+			</p>
+			<div>button</div>
+		</div>
+	);
+}

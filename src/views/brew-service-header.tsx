ed1642d921f3ba3d9639e6e80d217b44110d5a3c
J.yeu <chen@jyeu.xyz>
2024-12-28 12:30:08 +0800

    ✨ feat(brew-service-header): Add refresh button to header

--- src/views/brew-service-header.tsx
+++ src/views/brew-service-header.tsx
@@ -1,10 +1,32 @@
+import { Button } from "@nextui-org/react";
+import { useQueryClient } from "@tanstack/react-query";
+import { RotateCw } from "lucide-react";
+
 export function BrewServiceHeader() {
+	const queryClient = useQueryClient();
+
+	const handleRefetch = () => {
+		void queryClient.invalidateQueries({
+			queryKey: ["brewServiceList"],
+		});
+	};
+
 	return (
-		<div className="mb-4 flex flex-nowrap items-center justify-between">
-			<p className="relative bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text font-bold text-transparent">
+		<div className="mt-1 mb-4 flex flex-nowrap items-center justify-between">
+			<p className="relative bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text font-bold text-2xl text-transparent">
 				Homebrew Services
 			</p>
-			<div>button</div>
+			<div>
+				<Button
+					isIconOnly
+					aria-label="refresh"
+					size="sm"
+					variant="light"
+					onPress={handleRefetch}
+				>
+					<RotateCw color="#ccc" />
+				</Button>
+			</div>
 		</div>
 	);
 }
